9e185e1f0188838545f11f1124c7e4eb0fafe8d5
fried <fried.scholvinck@gmail.com>
2023-10-13 16:04:27 +0200

    test some stuff

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 let CardTypes = [
-{ name: "vue", image: "https://vuejs.org/images/logo.png" },
-{ name: "express", image: "https://coligo.io/images/express.svg" },
-{ name: "mongo", image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/45/MongoDB-Logo.svg/527px-MongoDB-Logo.svg.png" },
-{ name: "nodejs", image: "https://worldvectorlogo.com/logos/nodejs-icon.svg" },
-{ name: "webpack", image: "https://camo.githubusercontent.com/66747a6e05a799aec9c6e04a3e721ca567748e8b/68747470733a2f2f662e636c6f75642e6769746875622e636f6d2f6173736574732f313336353838312f313931383337332f32653035373166612d376462632d313165332d383436352d3839356632393164343366652e706e67" },
-{ name: "babel", image: "https://babeljs.io/images/logo.svg" }
-//{ name: "jade", image: "http://jade-lang.com/style/jade-logo-header.svg" },
+{ name: "harten", image: "assets/ace-of-hearts.png" },
+{ name: "klaver", image: "assets/ace-of-clubs.png" },
+{ name: "ruiten", image: "assets/ace-of-diamonds.png" },
+{ name: "schoppen", image: "assets/ace-of-spades.png" },
 ];
 
 let shuffleCards = () => {
